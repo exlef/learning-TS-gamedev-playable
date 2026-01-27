90209c036656ce5d74c218d5ca91fd218a98ba93
Amed <amed.duman@voodoo.io>
2026-01-27 22:05:24 +0300

    split project into multiple files

---
+++ src/game.ts
@@ -0,0 +1,55 @@
+import * as PIXI from 'pixi.js';
+import playerImg from './assets/player.png';
+
+export class Game{
+    private readonly app : PIXI.Application;
+    player! : PIXI.Sprite;
+
+    constructor() {
+        this.app = new PIXI.Application();
+    }
+
+    async startGame() : Promise<void>{
+        await this.app.init({resizeTo: window, backgroundColor: 0x1099bb});
+        document.body.appendChild(this.app.canvas);
+
+        await this.loadAssets();
+        this.createScene();
+
+        this.app.ticker.add((time) => {
+
+        });
+    }
+
+    private async loadAssets(){
+        await PIXI.Assets.load(playerImg); // We just ensure the asset is loaded into the cache here
+    }
+
+    private createScene():void{
+        const texture = PIXI.Assets.get(playerImg); // Retrieve texture from cache (it was loaded in loadAssets)
+        this.player = new PIXI.Sprite(texture);
+        this.player.x = this.app.screen.width / 2;
+        this.player.y = this.app.screen.height / 2;
+        this.player.anchor.set(0.5, 0.5);
+
+        // input
+        this.player.eventMode = 'static';
+        this.player.cursor = 'pointer';
+
+        this.player.on('pointerdown', (event)=>{
+            console.log("I was clicked");
+            this.player.x = Math.random() * this.app.screen.width;
+            this.player.y = Math.random() * this.app.screen.height;
+            event.stopPropagation();
+        });
+
+
+        this.app.stage.addChild(this.player);
+        this.app.stage.eventMode = 'static';
+        this.app.stage.hitArea = this.app.screen;
+        this.app.stage.on('pointerdown', (event) => {
+            const clickPosition = event.global;
+            console.log("clicked at " + clickPosition.x + " " + clickPosition.y);
+        });
+    }
+}
